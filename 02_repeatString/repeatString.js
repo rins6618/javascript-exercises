d92f9f9826bd2c86a8fa72393780226921fe57d3
rins <47487086+rins6618@users.noreply.github.com>
2024-07-11 21:30:16 -0300

    Implement of 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
 const repeatString = function(str, num) {
-
+    if (num < 0) return "ERROR"
+    let result = "";
+    while (num --> 0) {
+        result += str;
+    }
+    return result;
 };
 
 // Do not edit below this line
