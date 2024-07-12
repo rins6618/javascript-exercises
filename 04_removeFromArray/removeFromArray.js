9fff0c6fcd786baae22d83a43bb771d3d1b7fe96
rins <47487086+rins6618@users.noreply.github.com>
2024-07-11 21:45:19 -0300

    Implement for 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    const filter = [...args];
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (filter.includes(arr[i])) continue;
+        result.push(arr[i]);
+    }
+    return result;
 };
 
 // Do not edit below this line
