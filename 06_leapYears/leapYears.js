921b070dd8509c1647b7ee0369e44f233d796aea
rins <47487086+rins6618@users.noreply.github.com>
2024-07-11 22:00:45 -0300

    Implement for 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function(inputYear) {
+    const centuryCheck = (inputYear % 100 === 0 && inputYear % 400 === 0);
+    const nonCentCheck = (inputYear % 4 === 0 && !(inputYear % 100 === 0) );
+    return nonCentCheck || centuryCheck;
 };
 
 // Do not edit below this line
