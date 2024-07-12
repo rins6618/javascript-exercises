73a5248a1f03080a331dbb2ffbc5f816e59cb362
rins <47487086+rins6618@users.noreply.github.com>
2024-07-11 21:53:18 -0300

    Implement for 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    const numCheck = (typeof num1 !== "number" || typeof num2 !== "number");
+    const negativeCheck = (num1 < 0 || num2 < 0);
 
+    if (numCheck || negativeCheck) return "ERROR";
+
+    const base = num1 < num2 ? num1 : num2;
+    const limit = num1 === base ? num2 : num1;
+    let sum = 0;
+
+    for (let i = base; i <= limit; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
