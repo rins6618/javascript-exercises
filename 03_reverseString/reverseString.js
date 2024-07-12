1f36bd2de3342e9f4539ffef0fb3ec5e5f30df27
rins <47487086+rins6618@users.noreply.github.com>
2024-07-11 21:33:41 -0300

    Implement for 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let result = "";
+    let len = str.length;
+    while (len --> 0) {
+        result += str[len];
+    }
+    return result;
 };
 
 // Do not edit below this line
