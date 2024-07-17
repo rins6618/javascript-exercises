a0963a5986af754785d1e8c3daec02846fc9c760
rins <47487086+rins6618@users.noreply.github.com>
2024-07-17 18:50:49 -0300

    Implement for 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (str) {
+
+    const VALID_CHARS = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+    const cleanStr = str
+        .toLowerCase()
+        .split("")
+        .filter(char => VALID_CHARS.includes(char))
+        .join("");
+
+    const reverseStr = cleanStr
+        .split("")
+        .toReversed()
+        .join("");
+    
+    return (cleanStr === reverseStr);
 
 };
+
+palindromes("Animal loots foliated detail of stool lamina.");
 
 // Do not edit below this line
 module.exports = palindromes;
