12383411345d8521e966e1c98f133dbfce92c966
rins <47487086+rins6618@users.noreply.github.com>
2024-07-17 18:29:12 -0300

    Implement for 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(x, y) {
+    return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+    if (!Array.isArray(arr) || arr.length === 0) return 0; 
+	return arr.reduce((sum, curr) => sum += curr);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+    return arr.reduce((sum, curr) => sum *= curr);
 };
 
-const power = function() {
-	
+const power = function(base, pow) {
+    // could solve with **
+    let result = 1;
+    for (let i = 1; i <= pow; i++) 
+        result *= base;
+    return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	const factorialValues = [1];
+    for (let i = 1; i <= num; i++) 
+        factorialValues.push(i);
+    return factorialValues.reduce( (last, curr) => last *= curr );
 };
 
 // Do not edit below this line
