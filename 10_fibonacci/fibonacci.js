7d58f637723005c422dce0130fdff3546e95b4ce
rins <47487086+rins6618@users.noreply.github.com>
2024-07-17 20:59:15 -0300

    Implement for 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(nthFib) {
+    let fibIdx = parseInt(nthFib);
+    if (fibIdx === 0) return 0;            
+    if (fibIdx < 0 || isNaN(fibIdx)) return "OOPS"; 
 
+    const fibSequence = [1, 1];
+    while (fibSequence.length < fibIdx) {
+        const slice = fibSequence.slice(-2);
+        const nextElement = slice.reduce( (sum, curr) => sum += curr);
+        fibSequence.push(nextElement);
+    }
+    return fibSequence[fibIdx - 1];
 };
 
 // Do not edit below this line
