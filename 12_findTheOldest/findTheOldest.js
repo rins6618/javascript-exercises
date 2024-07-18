0445386ad5c8bfb5c44a27a5bd51c6116f7be360
rins <47487086+rins6618@users.noreply.github.com>
2024-07-17 21:56:04 -0300

    Solution for 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+function getAge(person) {
+    let death;
+    if (!person.yearOfDeath) death = 2024;
+    else death = person.yearOfDeath;
+    return death - person.yearOfBirth;
+}
 
+const findTheOldest = function(peopleArray) {
+    return peopleArray.reduce((last, person) => {
+        let currentAge = getAge(person);
+        let lastAge = getAge(last);
+        console.log(currentAge, lastAge)
+        return currentAge > lastAge ? person : last;
+    });
 };
 
 // Do not edit below this line
